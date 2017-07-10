294057d5972f29a5e301fc465453da5f9ec27573
林小龙 <xiaolong.lin@ele.me>
2017-07-10 14:26:51 +0800

    feat: 接入微信公众平台验证

--- app_wechat.js
+++ app_wechat.js
@@ -1,3 +1,15 @@
-/**
- * Created by linxiaolong on 10/07/2017.
- */
+const express = require('express');
+const wechat = require('./wechat/wechat');
+const config = require('./config');
+
+var app = express();
+
+var wechatApp = new wechat(config);
+
+// 用于处理所有进入 3000 端口 get 的连接请求
+app.get('/', function(req, res) {
+  console.log('query', req.query);
+  wechatApp.auth(req, res);
+});
+
+module.exports = app;
